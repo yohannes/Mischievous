c57aef012ac210fc56b60ff66e9ee803f53cc53f
Yohannes Wijaya <readyforservice@icloud.com>
2016-07-05 16:06:39 +0700

    Added human fart sound effect. Added colors to all buttons' background. Imported AVFoundation. Implemented & connected all buttons to corresponding functions. Added logic to play sound effect.

--- Mischievous/ViewController.swift
+++ Mischievous/ViewController.swift
@@ -7,19 +7,66 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    // MARK: - Stored Properties
+    
+    var audioPlayer: AVAudioPlayer?
+//    var soundEffectDirectoryPath: NSURL?
 
+    // MARK: - IBAction Properties
+    
+    @IBAction func fartButtonDidTouch(sender: UIButton) {
+//        self.playSoundEffect("Human_Fart.mp3")
+        self.playSoundEffect("Human_Fart")
+    }
+    
+    @IBAction func burpButtonDidTouch(sender: UIButton) {
+        print("burping....")
+    }
+
+    @IBAction func coughButtonDidTouch(sender: UIButton) {
+        print("coughing....")
+    }
+    
+    @IBAction func yawnButtonDidTouch(sender: UIButton) {
+        print("yawning....")
+    }
+    
+    @IBAction func sneezeButtonDidTouch(sender: UIButton) {
+        print("sneezing....")
+    }
+    
+    @IBAction func screamButtonDidTouch(sender: UIButton) {
+        print("screaming....")
+    }
+    
+    // MARK: - Helper Methods
+    
+    private func playSoundEffect(fileName: String) {
+        guard let validSoundFile = NSBundle.mainBundle().pathForResource(fileName, ofType: "mp3"), validSoundFileURL = NSURL(string: validSoundFile) else {
+            return
+        }
+    
+        do {
+            self.audioPlayer = try AVAudioPlayer(contentsOfURL: validSoundFileURL)
+        }
+        catch {
+            print("there's an error initializing an instance of AVAudioPlayer")
+        }
+
+        self.audioPlayer?.play()
+    }
+    
+    // MARK: - ViewController Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.audioPlayer?.delegate = self
+        self.audioPlayer?.prepareToPlay()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
