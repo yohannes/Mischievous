17df5260596765aefd6b75b297aa12e15fa01efe
Yohannes Wijaya <readyforservice@icloud.com>
2016-10-19 23:25:20 +0800

    Replaced LinearProgressBar 3rd party library with KYNavigationProgress to animate audio playback.

--- Mischievous/ViewController.swift
+++ Mischievous/ViewController.swift
@@ -10,82 +10,87 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController, AVAudioPlayerDelegate {
-    
-    // MARK: - Stored Properties
-    
-    var audioPlayer: AVAudioPlayer?
-    var linearProgressBar: LinearProgressBar!
-
-    // MARK: - IBAction Properties
-    
-    @IBAction func fartButtonDidTouch(_ sender: UIButton) {
-        self.playSoundEffect("Human_Fart")
-    }
-    
-    @IBAction func burpButtonDidTouch(_ sender: UIButton) {
-        self.playSoundEffect("Male_Burps")
-    }
-
-    @IBAction func coughButtonDidTouch(_ sender: UIButton) {
-        self.playSoundEffect("Male_Cough")
-    }
-    
-    @IBAction func laughButtonDidTouch(_ sender: UIButton) {
-        self.playSoundEffect("Male_Laugh")
-    }
-    
-    @IBAction func sneezeButtonDidTouch(_ sender: UIButton) {
-        self.playSoundEffect("Male_Sneeze")
-    }
-    
-    @IBAction func cryButtonDidTouch(_ sender: UIButton) {
-        self.playSoundEffect("Male_Crying")
-    }
-    
-    // MARK: - Helper Methods
-    
-    fileprivate func playSoundEffect(_ fileName: String) {
-        guard let validSoundFile = Bundle.main.path(forResource: fileName, ofType: "mp3"), let validSoundFileURL = URL(string: validSoundFile) else {
-            return
-        }
-    
-        do {
-            self.audioPlayer = try AVAudioPlayer(contentsOf: validSoundFileURL)
-            self.audioPlayer?.delegate = self
-            self.linearProgressBar.startAnimation()
-        }
-        catch {
-            print("there's an error initializing an instance of AVAudioPlayer")
-        }
-
-        self.audioPlayer?.play()
-    }
-    
-    // MARK: - ViewController Methods
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        self.audioPlayer?.prepareToPlay()
-      
-        self.linearProgressBar = LinearProgressBar()
-        self.configureLinearProgressBar()
-    }
   
-    // MARK: - AVAudioPlayerDelegate Methods
+  // MARK: - Stored Properties
   
-  func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
-    if flag {
-      self.linearProgressBar.stopAnimation()
-    }
+  var audioPlayer: AVAudioPlayer?
+  var timer: Timer!
+  var audioDuration: TimeInterval!
+  
+  // MARK: - IBAction Properties
+  
+  @IBAction func fartButtonDidTouch(_ sender: UIButton) {
+    self.playSoundEffect("Human_Fart")
   }
   
-    // MARK: - Private Methods
+  @IBAction func burpButtonDidTouch(_ sender: UIButton) {
+    self.playSoundEffect("Male_Burps")
+  }
   
-    fileprivate func configureLinearProgressBar(){
-      linearProgressBar.backgroundColor = UIColor(red:0.68, green:0.81, blue:0.72, alpha:1.0)
-      linearProgressBar.progressBarColor = UIColor(red:0.26, green:0.65, blue:0.45, alpha:1.0)
-      linearProgressBar.heightForLinearBar = 5
+  @IBAction func coughButtonDidTouch(_ sender: UIButton) {
+    self.playSoundEffect("Male_Cough")
+  }
+  
+  @IBAction func laughButtonDidTouch(_ sender: UIButton) {
+    self.playSoundEffect("Male_Laugh")
+  }
+  
+  @IBAction func sneezeButtonDidTouch(_ sender: UIButton) {
+    self.playSoundEffect("Male_Sneeze")
+  }
+  
+  @IBAction func cryButtonDidTouch(_ sender: UIButton) {
+    self.playSoundEffect("Male_Crying")
+  }
+  
+  // MARK: - Helper Methods
+  
+  fileprivate func playSoundEffect(_ fileName: String) {
+    guard let validSoundFile = Bundle.main.path(forResource: fileName, ofType: "mp3"), let validSoundFileURL = URL(string: validSoundFile) else {
+      return
     }
+    
+    do {
+      self.audioPlayer = try AVAudioPlayer(contentsOf: validSoundFileURL)
+      self.audioPlayer?.delegate = self
+      
+      self.audioDuration = self.audioPlayer?.duration
+      timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.incrementProgressBar), userInfo: nil, repeats: true)
+    }
+    catch {
+      print("there's an error initializing an instance of AVAudioPlayer")
+    }
+    
+    self.audioPlayer?.play()
+  }
+  
+  // MARK: - ViewController Methods
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    self.audioPlayer?.prepareToPlay()
+    
+    self.navigationController?.progressHeight = 3
+    self.navigationController?.progressTintColor = UIColor(red: 0, green: 0.463, blue: 1, alpha: 1)
+  }
+  
+  // MARK: - AVAudioPlayerDelegate Methods
+  
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+      if flag {
+        self.navigationController?.finishProgress()
+        self.timer.invalidate()
+      }
+    }
+  
+  // MARK: - Private Methods
+  
+  func incrementProgressBar() {
+    guard let validProgress = self.navigationController?.progress else { return }
+    print("************\(self.navigationController?.progress)")
+    self.navigationController?.setProgress(validProgress + Float(1 / self.audioDuration), animated: true)
+  }
+
 }
 
--- README.md
+++ README.md
@@ -5,3 +5,6 @@ An iOS app that lists and plays prank sound effects
 
 Image hosted in [IMGUR](http://imgur.com)
 
+3rd Party Libraries Used in this app:
+- [KYNavigationProgress](https://github.com/ykyouhei/KYNavigationProgress) by [ykyouhei](https://github.com/ykyouhei/)
+
